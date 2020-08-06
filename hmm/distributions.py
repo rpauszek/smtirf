46cb8ab48bf6310095c492a081652acd525b0076
rpauszek <ray.pauszek@gmail.com>
2020-08-06 15:42:33 -0700

    copied in Normal and NormalSharedVariance properties

--- hmm/distributions.py
+++ hmm/distributions.py
@@ -9,8 +9,10 @@ from scipy.special import gammaln, digamma
 from . import row, col, normalize_rows
 
 class Dirichlet():
+    _NDIM = 1
 
     def __init__(self, alpha):
+        assert alpha.ndim == self._NDIM
         self._alpha = alpha
 
     @property
@@ -27,7 +29,7 @@ class Dirichlet():
 
     @property
     def mu(self):
-        return normalize_rows(self._alpha)
+        return normalize_rows(self.alpha)
 
     @property
     def lnMuStar(self):
@@ -38,6 +40,7 @@ class Dirichlet():
 
 
 class DirichletArray(Dirichlet):
+    _NDIM = 2
 
     @property
     def K(self):
@@ -53,15 +56,52 @@ class DirichletArray(Dirichlet):
 
 
 class Normal():
+    """ N(x|μ,τ) """
 
-    def __init__(self):
-        pass
+    def __init__(self, mu, tau):
+        assert mu.ndim == 1
+        assert tau.ndim == 1
+        self._mu = mu
+        self._tau = tau
+
+    @property
+    def K(self):
+        return self._mu.size
+
+    @property
+    def mu(self):
+        return self._mu
+
+    @property
+    def tau(self):
+        return self._tau
+
+    @property
+    def var(self):
+        return 1/self.tau
+
+    @property
+    def sigma(self):
+        return np.sqrt(1/self.tau)
+
+    def p_X(self, x):
+        """ P(x|μ,τ) """
+        X = row(x) - col(self.mu)
+        lnP = -0.5 * np.log(2*np.pi/col(self.tau)) - col(self.tau)/2 * X**2
+        return np.exp(lnP)
 
 
-class NormalSharedVariance():
+class NormalSharedVariance(Normal):
 
-    def __init__(self):
-        pass
+    def __init__(self, mu, tau):
+        assert mu.ndim == 1
+        assert not isinstance(tau, (np.ndarray, list, tuple))
+        self._mu = mu
+        self._tau = tau
+
+    # @property
+    # def tau(self):
+    #     return np.full(self.K, self._tau)
 
 
 class NormalGamma():
