3da514ab840cf9e422d85a378c2eae32682f9887
Ray Pauszek <ray.pauszek@gmail.com>
2020-08-29 20:43:57 -0700

    simplified trace viewer canvases --> removed BaseTraceViewerPlot and rolled in switchable canvas prototyped in FretExperimentViewerPlot into final TraceViewerPlot

--- gui/plots/canvases_traceviewer.py
+++ gui/plots/canvases_traceviewer.py
@@ -8,9 +8,9 @@ import matplotlib as mpl
 
 from . import QtCanvas, ScrollableCanvas, InteractiveCanvas
 
-__all__ = ["FretExperimentViewerPlot"]
+__all__ = ["TraceViewerPlot"]
 
-class BaseTraceViewerPlot(ScrollableCanvas, InteractiveCanvas):
+class TraceViewerPlot(ScrollableCanvas, InteractiveCanvas):
 
     def connect(self):
         super().connect()
@@ -19,23 +19,6 @@ class BaseTraceViewerPlot(ScrollableCanvas, InteractiveCanvas):
         self.controller.selectedEdited.connect(self.update_plots)
         self.mpl_connect('motion_notify_event', self.controller.motion_notify)
         self.mpl_connect('button_release_event', self.on_release)
-
-    def update_plots(self, trc):
-        for ax in self.fig.axes:
-            try:
-                ax.set_trace(trc)
-            except AttributeError:
-                pass
-        self.draw()
-
-    def on_release(self, evt):
-        pass
-
-
-class FretExperimentViewerPlot(BaseTraceViewerPlot):
-
-    def __init__(self, controller):
-        super().__init__(controller)
         self.controller.experimentLoaded.connect(self.refresh)
 
     def refresh(self, expt):
@@ -50,10 +33,14 @@ class FretExperimentViewerPlot(BaseTraceViewerPlot):
         self.set_zoom_axes(self.ax1, button=1)
         self.set_baseline_axes(self.ax3, button=3)
         self.draw()
-        # self.layout()
 
-    # def layout(self):
-        # gs = GridSpec(3, 1)
-        # self.ax1 = self.fig.add_subplot(gs[0], projection="fret")
-        # self.ax2 = self.fig.add_subplot(gs[1], projection="donoracceptor")
-        # self.ax3 = self.fig.add_subplot(gs[2], projection="total")
+    def update_plots(self, trc):
+        for ax in self.fig.axes:
+            try:
+                ax.set_trace(trc)
+            except AttributeError:
+                pass
+        self.draw()
+
+    def on_release(self, evt):
+        pass
