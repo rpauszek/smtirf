3a245c93135b070931821517f902c95b2bf8f06c
Ray Pauszek <r.pauszek@lumicks.com>
2021-08-22 15:38:12 +0200

    axes: InteractiveTraceViewer create axes on experiment load

--- gui/canvases.py
+++ gui/canvases.py
@@ -14,27 +14,34 @@ from .axes import TraceAxes
 class QtCanvas(FigureCanvas):
 
     def __init__(self):
-        self.fig = Figure()
-        super().__init__(self.fig)
+        self.figure = Figure()
+        super().__init__(self.figure)
         bgcolor = QtWidgets.QWidget().palette().color(QPalette.Background)
-        self.fig.set_facecolor(np.array(bgcolor.getRgb()[:-1])/255)
-        self.fig.set_tight_layout(True)
+        self.figure.set_facecolor(np.array(bgcolor.getRgb()[:-1])/255)
+        self.figure.set_tight_layout(True)
 
 
 class InteractiveTraceViewer(QtCanvas):
 
     traceChanged = pyqtSignal(object)
+    axes = {"selection": None,
+            "channels": None,
+            "total": None}
 
     def __init__(self, controller):
         super().__init__()
         self.controller = controller
+        self.gs = self.figure.add_gridspec(3, 1)
 
-        gs = self.fig.add_gridspec(3, 1)
-        self.axSelection = self.fig.add_subplot(gs[0], projection=TraceAxes(parent=self, dataType="selection"))
-        self.axChannels = self.fig.add_subplot(gs[1], projection=TraceAxes(parent=self, dataType="channels"))
-        self.axTotal = self.fig.add_subplot(gs[2], projection=TraceAxes(parent=self, dataType="total"))
-
+        controller.experimentChanged.connect(self.make_axes)
         controller.traceChanged.connect(self.update_plots)
+
+    def make_axes(self):
+        for j, (dataType, ax) in enumerate(self.axes.items()):
+            if ax is not None:
+                ax.remove()
+            projection = TraceAxes(parent=self, dataType=dataType)
+            ax = self.figure.add_subplot(self.gs[j], projection=projection)
 
     def update_plots(self, trace):
         self.traceChanged.emit(trace)
