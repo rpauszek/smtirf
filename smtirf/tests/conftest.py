2bea964b9e9b21d2ea4d75c9b3632eed31b3a1ff
Ray Pauszek <r.pauszek@lumicks.com>
2022-10-09 09:27:59 +0200

    tests: add test_hmm.py
    verify that simple HMM fitting is deterministic given default initial parameter guesses

--- smtirf/tests/conftest.py
+++ smtirf/tests/conftest.py
@@ -24,6 +24,45 @@ def fret_pma_file(tmpdir_factory):
             "channel_sigma": 30,
             "frame_size": 512,
             "channel_border": 20,
+            "seed": 1234,
+        },
+        "log_params": {
+            "gain": 300,
+            "data_scaler": 800,
+            "frame_length": 100,
+            "record_date": datetime.now().strftime("%a %b %d %H:%M:%S %Y"),
+        },
+    }
+
+    statepaths, *data = simulate_fret_experiment(
+        params["K"], params["n_frames"], params["n_spots"], **params["hmm_params"]
+    )
+
+    write_pma_data(
+        savebase, *data, params["n_frames"], params["n_spots"], params["log_params"]
+    )
+
+    return savebase, statepaths
+
+
+@pytest.fixture(scope="session")
+def fret_short_file(tmpdir_factory):
+    tmpdir = Path(str(tmpdir_factory.mktemp("smtirf")))
+    savebase = tmpdir / "short3"
+
+    params = {
+        "K": 3,
+        "n_frames": 25,
+        "n_spots": 5,
+        "hmm_params": {
+            "transition_diag": 10,
+            "frame_length": 0.100,
+            "signal_lifetime": 80,
+            "total_intensity": 500,
+            "channel_sigma": 30,
+            "frame_size": 512,
+            "channel_border": 20,
+            "seed": 1234,
         },
         "log_params": {
             "gain": 300,
---
+++ smtirf/tests/test_hmm.py
@@ -0,0 +1,50 @@
+import numpy as np
+from smtirf import Experiment
+
+
+# fmt: off
+ref_state_path = np.array(
+    [0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 1, 1, 1, 0, 0, 2]
+)
+ref_emission_path = np.array(
+    [-0.05613604, -0.05613604, -0.05613604, -0.05613604, -0.05613604, -0.05613604,
+     -0.05613604, -0.05613604,  1.00375579,  1.00375579,  1.00375579,  1.00375579,
+      1.00375579,  1.00375579,  1.00375579,  1.00375579,  1.00375579, -0.05613604,
+     -0.05613604,  0.45393879,  0.45393879,  0.45393879, -0.05613604, -0.05613604,
+      1.00375579]
+)
+# fmt: on
+
+
+def test_deterministic_hmm(fret_short_file):
+    filename_base, statepaths = fret_short_file
+    experiment = Experiment.from_pma(filename_base.with_suffix(".traces"), "fret")
+
+    trc = experiment[0]
+    trc.train(
+        "em",
+        3,
+        sharedVariance=False,
+        refineByKmeans=False,
+        maxIter=1000,
+        tol=1e-5,
+        printWarnings=False,
+    )
+    state_path = trc.SP
+    emission_path = trc.EP
+
+    np.testing.assert_equal(state_path, ref_state_path)
+    np.testing.assert_almost_equal(emission_path, ref_emission_path)
+
+    for _ in range(5):
+        trc.train(
+            "em",
+            3,
+            sharedVariance=False,
+            refineByKmeans=False,
+            maxIter=1000,
+            tol=1e-5,
+            printWarnings=False,
+        )
+        np.testing.assert_equal(trc.SP, state_path)
+        np.testing.assert_equal(trc.EP, emission_path)
