32ac3a8bd69f7b76e4f1d1b3539fea8cc1101898
Ray Pauszek <r.pauszek@lumicks.com>
2023-01-01 16:18:43 +0100

    models: deduplicate expectation step in separate function

--- smtirf/hmm/models.py
+++ smtirf/hmm/models.py
@@ -22,19 +22,50 @@ class HiddenMarkovModel(AsDictMixin):
         phi = Normal.initialize_kmeans(K, x, shared_variance)
         return cls(K, pi, A, phi)
 
-    def train(self, *observations, max_iter=250, tol=1e-5):
+    @staticmethod
+    def _expectation_step(pi, A, pdf, observations):
+        """Evaluate the Expectation Step of the Baul-Welch algorithm on a set of observations.
 
-        results = [fwdback(self.pi.mu, self.A.mu, self.phi.pdf(x).T) for x in observations]
-
+        Parameters
+        ----------
+        pi : np.ndarray
+            Initial state probabilities.
+        A : np.ndarray
+            State transition probability matrix.
+        pdf : callable
+            Probability Distribution Function for the emission model.
+        observations : list
+            List of observation arrays.
+        """
+        results = [fwdback(pi, A, pdf(x).T) for x in observations]
         gammas = [r[0] for r in results]
         xis = [r[1] for r in results]
-        Ls = [r[2] for r in results]
+        log_likelihoods = [r[2] for r in results]
+        return gammas, xis, log_likelihoods
 
-        theta = HiddenMarkovModel(self.K, self.pi, self.A, self.phi, ExitFlag(np.sum(Ls)))
-        print("***", theta.exit_flag)
+    def train(self, *observations, max_iter=250, tol=1e-5):
+        """Train the model using the Baul-Welch algorithm.
+
+        Parameters
+        ----------
+        observations
+            Variable length set of observation arrays.
+        max_iter : int
+            Maximum number of iterations.
+        tol : float
+            Convergence tolerance.
+        """
+        gammas, xis, log_likelihoods = HiddenMarkovModel._expectation_step(
+            self.pi.mu, self.A.mu, self.phi.pdf, observations
+        )
+        theta = HiddenMarkovModel(
+            self.K, self.pi, self.A, self.phi, ExitFlag(np.sum(log_likelihoods))
+        )
 
         for itr in range(max_iter):
-            theta, gammas, xis, Ls = theta._update(observations, gammas, xis, tol=tol)
+            theta, gammas, xis, log_likelihoods = theta._update(
+                observations, gammas, xis, tol=tol
+            )
             print(itr)
             print(theta.exit_flag)
             if theta.exit_flag.is_converged:
@@ -43,7 +74,19 @@ class HiddenMarkovModel(AsDictMixin):
         return theta
 
     def _update(self, observations, gammas, xis, tol=1e-5):
-        # observations : list
+        """Update the model from previous E-step.
+
+        Parameters
+        ----------
+        observations : list
+            List of observation arrays.
+        gammas : list
+            List of gamma parameter arrays; calculated from forward-backward algorithm.
+        xis : list
+            List of xi parameter arrays; calculated from forward-backward algorithm.
+        tol : floag
+            Convergence tolerance.
+        """
 
         gamma0 = np.vstack([gamma[0] for gamma in gammas]).sum(axis=0) / len(gammas)
         xi_sum = np.stack(xis, axis=2).sum(axis=2)
@@ -55,18 +98,14 @@ class HiddenMarkovModel(AsDictMixin):
         phi = self.phi.update(np.hstack(observations), np.vstack(gammas))
 
         # E-Step
-        results = [fwdback(pi.mu, A.mu, phi.pdf(x).T) for x in observations]
-
-        gammas = [r[0] for r in results]
-        xis = [r[1] for r in results]
-        Ls = [r[2] for r in results]
-
-        return (
-            HiddenMarkovModel(self.K, pi, A, phi, self.exit_flag.step(np.sum(Ls), tol=tol)),
-            gammas,
-            xis,
-            Ls,
+        gammas, xis, log_likelihoods = HiddenMarkovModel._expectation_step(
+            pi.mu, A.mu, phi.pdf, observations
         )
+
+        exit_flag = self.exit_flag.step(np.sum(log_likelihoods), tol=tol)
+        theta = HiddenMarkovModel(self.K, pi, A, phi, exit_flag)
+
+        return (theta, gammas, xis, log_likelihoods)
 
     def label(self, x):
         return viterbi(self.pi.mu, self.A.mu, self.phi.pdf(x).T).astype(int)
@@ -76,7 +115,9 @@ def _construct_from_old_version(model):
     """Construct a HiddenMarkovModel from old JSON format, <v0.2.0"""
 
     if model["modelType"] != "em":
-        raise NotImplementedError("Only Classic (Expectation-Maximization) models are supported currently.")
+        raise NotImplementedError(
+            "Only Classic (Expectation-Maximization) models are supported currently."
+        )
 
     K = int(model["K"])
     pi = Categorical(K, np.array(model["pi"]))
