5a50826a27552b688b7bc6227bc4112b572c3908
Ray Pauszek <r.pauszek@lumicks.com>
2023-01-14 13:45:37 +0100

    app_main: simplify main app layout

--- smtirf/gui/app_main.py
+++ smtirf/gui/app_main.py
@@ -37,35 +37,27 @@ class SMTirfMainWindow(QMainWindow):
 
     def layout(self):
         panel = QtWidgets.QWidget()
-        hbox = QtWidgets.QHBoxLayout()
-        panel.setLayout(hbox)
         self.setCentralWidget(panel)
 
-        # *** plot/navigation panel
-        vbox = QtWidgets.QVBoxLayout()
-        canvas = InteractiveTraceViewer(self.controller)
-        vbox.addWidget(canvas, stretch=1)
+        main_grid = QtWidgets.QGridLayout()
+        main_grid.addWidget(InteractiveTraceViewer(self.controller), 0, 0)
+        main_grid.addWidget(widgets.TraceIndexSlider(self.controller), 1, 0)
 
-        navbar = widgets.TraceIndexSlider(self.controller)
-        vbox.addWidget(navbar)
-
-        hbox.addLayout(vbox, stretch=1)
-
-        # *** right side panel
         right_vbox = QtWidgets.QVBoxLayout()
         right_vbox.addSpacerItem(QSpacerItem(200, 0, QSizePolicy.Expanding, QSizePolicy.Fixed))
-        hbox.addLayout(right_vbox)
-
         right_vbox.addWidget(
             panels.TraceGroup(self.controller, self.controller.experimentChanged)
         )
         right_vbox.addWidget(
             panels.ExperimentGroup(self.controller, self.controller.experimentChanged)
         )
-
-        # *** coordinate label
         right_vbox.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Fixed, QSizePolicy.Expanding))
         right_vbox.addWidget(widgets.CoordinateLabel(self.controller))
+        main_grid.addLayout(right_vbox, 0, 1, 2, 1)
+
+        main_grid.setRowStretch(0, 1)
+        main_grid.setColumnStretch(0, 1)
+        panel.setLayout(main_grid)
 
     def import_pma_experiment(self):
         dlg = ImportPmaDialog()
